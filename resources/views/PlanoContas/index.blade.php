193feacf007a778191980c5e47f12c61275ebe33
falchiprf <pedroroberto@falchi.com.br>
2023-03-27 16:55:55 -0300

    Update index.blade.php
    PERMISSOES EM PLANOCONTAS

--- resources/views/PlanoContas/index.blade.php
+++ resources/views/PlanoContas/index.blade.php
@@ -1,140 +1,166 @@
-
 @extends('layouts.bootstrap5')
 @section('content')
-<div class="py-5 bg-light">
-    <div class="container">
-          {{-- <nav aria-label="breadcrumb">
+    <div class="py-5 bg-light">
+        <div class="container">
+            {{-- <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
               <li class="breadcrumb-item"><a href="#">Permissions</a></li>
               <li class="breadcrumb-item active" aria-current="page">edit</li>
             </ol>
           </nav> --}}
 
-        <div class="card">
-    <h1 class="text-center">Plano de contas padrão para contabilidade</h1>
-    <hr>
+            <div class="card">
+                <h1 class="text-center">Plano de contas padrão para contabilidade</h1>
+                <hr>
+                {{-- @cannot('PLANO DE CONTAS - LISTAR')
+                    <li>
+                        <a href="/dashboard" data-bs-toggle="tooltip" data-bs-placement="center"
+                            data-bs-custom-class="custom-tooltip" data-bs-title="Clique e vá para o início do sistema"
+                            class="botton-link text-black">
+                            <i class="fa-solid fa-house"></i>
+                            <a href="{{ route('dashboard') }}" class="btn btn-danger btn-lg enabled" tabindex="-1"
+                                role="button" aria-disabled="true">SEM PERMISSÃO PARA ESTE SERVIÇO. CONSULTE O ADMINISTRADOR.
+                                Clique e vá para o início do sistema</a>
+                        </a>
+                    </li>
+                @endcan --}}
+
+
+                @can('PLANO DE CONTAS - INCLUIR')
+                    <a href="{{ route('PlanoContas.create') }}" class="btn btn-primary btn-lg enabled" tabindex="-1"
+                        role="button" aria-disabled="true">Incluir contas no plano de contas padrão</a>
+                @endcan
+                <p>Total de contas: {{ $linhas }}</p>
+
+                <table class="table table-bordered">
+
+                    <tr>
+                        <th>Descrição</th>
+                        <th>Tipo</th>
+                        <th>Código</th>
+                        <th>Grau</th>
+                        <th>Bloqueio</th>
+                        <th>Bloqueia datas anteriores a</th>
+                    </tr>
+                    @foreach ($cadastros as $cadastro)
+                        <tr>
+
+                            @if ($cadastro->Grau == '1')
+                                <td style="padding-left: 10px; Color:red; font-size: 30px;">
+                                    {{ $cadastro->Descricao }}
+                                </td>
+                            @endif
+
+
+                            @if ($cadastro->Grau == '2')
+                                <td style="padding-left: 60px;">
+                                    {{ $cadastro->Descricao }}
+                                </td>
+                            @endif
+                            @if ($cadastro->Grau == '3')
+                                <td style="padding-left: 90px;">
+                                    {{ $cadastro->Descricao }}
+                                </td>
+                            @endif
+                            @if ($cadastro->Grau == '4')
+                                <td style="padding-left: 120px;">
+                                    {{ $cadastro->Descricao }}
+                                </td>
+                            @endif
+                            @if ($cadastro->Grau == '5')
+                                <td style="padding-left: 150px; color:Blue;; font-size: 20px;">
+                                    {{ $cadastro->Descricao }}
+                                </td>
+                            @endif
 
 
 
-    <a href="{{ route('PlanoContas.create') }}" class="btn btn-primary btn-lg enabled" tabindex="-1" role="button"
-        aria-disabled="true">Incluir contas no plano de contas padrão</a>
-
-    <p>Total de contas: {{ $linhas}}</p>
-
-    <table class="table table-bordered">
-
-        <tr>
-            <th>Descrição</th>
-            <th>Tipo</th>
-            <th>Código</th>
-            <th>Grau</th>
-            <th>Bloqueio</th>
-            <th>Bloqueia datas anteriores a</th>
-        </tr>
-        @foreach ($cadastros as $cadastro)
-            <tr>
-
-                        @if($cadastro->Grau == "1")
-                            <td style="padding-left: 10px; Color:red; font-size: 30px;">
-                                {{ $cadastro->Descricao }}
+                            <td>
+                                {{ $cadastro->Tipo }}
                             </td>
-                        @endif
-
-
-                        @if($cadastro->Grau == "2")
-                            <td style="padding-left: 60px;">
-                                {{ $cadastro->Descricao }}
+                            <td>
+                                {{ $cadastro->Codigo }}
                             </td>
-                        @endif
-                        @if($cadastro->Grau == "3")
-                            <td style="padding-left: 90px;">
-                                {{ $cadastro->Descricao }}
+                            <td>
+                                {{ $cadastro->Grau }}
                             </td>
-                        @endif
-                        @if($cadastro->Grau == "4")
-                            <td style="padding-left: 120px;">
-                                {{ $cadastro->Descricao }}
+                            <td>
+                                {{ $cadastro->Bloqueio }}
                             </td>
-                        @endif
-                        @if($cadastro->Grau == "5")
-                            <td style="padding-left: 150px; color:Blue;; font-size: 20px;">
-                                {{ $cadastro->Descricao }}
+
+
+                            <td>
+                                @php
+                                    $Altera = DateTime::createFromFormat('Y-m-d', $cadastro->Bloqueiodataanterior);
+                                    if ($Altera instanceof DateTime) {
+                                        echo $Altera->format('d-m-Y');
+                                    } else {
+                                        echo ' ';
+                                    }
+                                @endphp
                             </td>
-                        @endif
 
+                            <td>
+                                <div class="row mt-2">
+                                    <div class="col-6">
+                                        @can('PLANO DE CONTAS - EDITAR')
+                                            <a href="{{ route('PlanoContas.edit', $cadastro->ID) }}"
+                                                class="btn btn-success btn-sm enabled" tabindex="-1" role="button"
+                                                aria-disabled="true">Editar</a>
+                                        @endcan
 
+                                        @can('PLANO DE CONTAS - EXCLUIR')
+                                            <form method="POST" action="{{ route('PlanoContas.destroy', $cadastro->ID) }}">
+                                                @csrf
+                                                <input type="hidden" name="_method" value="DELETE">
+                                                <button class="btn btn-danger btn-sm enabled" tabindex="-1" role="button"
+                                                    aria-disabled="true">Excluir</button>
+                                            </form>
+                                        @endcan
 
-                <td>
-                    {{ $cadastro->Tipo }}
-                </td>
-                <td>
-                    {{ $cadastro->Codigo }}
-                </td>
-                <td>
-                    {{ $cadastro->Grau }}
-                </td>
-                <td>
-                    {{ $cadastro->Bloqueio }}
-                </td>
+                                        @can('PLANO DE CONTAS - VER')
+                                            <a href="{{ route('PlanoContas.show', $cadastro->ID) }}"
+                                                class="btn btn-info btn-sm enabled" tabindex="-1" role="button"
+                                                aria-disabled="true">Ver</a>
+                                        @endcan
+                                    </div>
+                                </div>
+                            </td>
 
+                        </tr>
+                    @endforeach
+                </table>
+            @endsection
 
-                <td>
-                    @php
-                        $Altera = DateTime::createFromFormat("Y-m-d", $cadastro->Bloqueiodataanterior);
-                        if ($Altera instanceof DateTime) {
-                            echo $Altera->format('d-m-Y');
-                        } else {
-                            echo " ";
-                        }
-                    @endphp
-                </td>
+            @push('scripts')
+                <link rel="stylesheet"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
 
-                <td>
-                    <div class="row mt-2">
-                        <div class="col-6">
-
-                            <a href="{{ route('PlanoContas.edit', $cadastro->ID) }}" class="btn btn-success btn-sm enabled"
-                                tabindex="-1" role="button" aria-disabled="true">Editar</a>
-                            <form method="POST" action="{{ route('PlanoContas.destroy', $cadastro->ID) }}">
-                                @csrf
-                                <input type="hidden" name="_method" value="DELETE">
-                                <button class="btn btn-danger btn-sm enabled" tabindex="-1" role="button"
-                                    aria-disabled="true">Excluir</button>
-                            </form>
-
-                            <a href="{{ route('PlanoContas.show', $cadastro->ID) }}" class="btn btn-info btn-sm enabled"
-                                tabindex="-1" role="button" aria-disabled="true">Ver</a>
-                        </div>
-                    </div>
-                </td>
-
-            </tr>
-
-
-        @endforeach
-    </table>
-@endsection
-
-@push('scripts')
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
-
-    <script>
-        $('form').submit(function(e) {
-            e.preventDefault();
-            $.confirm({
-                title: 'Confirmar!',
-                content: 'Confirma a exclusão? Não terá retorno.',
-                buttons: {
-                    confirmar: function() {
-                        // $.alert('Confirmar!');
+                <script>
+                    $('form').submit(function(e) {
+                        e.preventDefault();
                         $.confirm({
                             title: 'Confirmar!',
-                            content: 'Deseja realmente continuar com a exclusão? Não terá retorno.',
+                            content: 'Confirma a exclusão? Não terá retorno.',
                             buttons: {
                                 confirmar: function() {
                                     // $.alert('Confirmar!');
-                                    e.currentTarget.submit()
+                                    $.confirm({
+                                        title: 'Confirmar!',
+                                        content: 'Deseja realmente continuar com a exclusão? Não terá retorno.',
+                                        buttons: {
+                                            confirmar: function() {
+                                                // $.alert('Confirmar!');
+                                                e.currentTarget.submit()
+                                            },
+                                            cancelar: function() {
+                                                // $.alert('Cancelar!');
+                                            },
+
+                                        }
+                                    });
+
                                 },
                                 cancelar: function() {
                                     // $.alert('Cancelar!');
@@ -142,15 +168,6 @@
 
                             }
                         });
-
-                    },
-                    cancelar: function() {
-                        // $.alert('Cancelar!');
-                    },
-
-                }
-            });
-        });
-    </script>
-@endpush
-
+                    });
+                </script>
+            @endpush
