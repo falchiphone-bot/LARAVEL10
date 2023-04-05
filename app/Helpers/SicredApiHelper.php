876daebd4cf0153a6105fe514cdebbf8cba27d64
Maurício Góes Pedro <mauriciomgp5@gmail.com>
2023-04-04 23:56:10 -0300

    AuthSicredi com Cache e TIMEOUT

--- app/Helpers/SicredApiHelper.php
+++ app/Helpers/SicredApiHelper.php
@@ -4,13 +4,14 @@ namespace App\Helpers;
 
 use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Request;
 
 class SicredApiHelper
 {
-    public static function auth($codigoBeneficiario, $codigocooperativa, $refres_token = false)
+    public static function auth($codigoBeneficiario, $codigocooperativa, $update_token = false)
     {
-        if ($refres_token) {
+        if ($update_token) {
             $auth = Http::asForm()
                 ->withBasicAuth(config('services.sicredi.client_id'), config('services.sicredi.secret_id'))
                 ->withHeaders([
@@ -19,25 +20,25 @@ class SicredApiHelper
                 ])
                 ->post('https://api-parceiro.sicredi.com.br/auth/openapi/token', [
                     'grant_type' => 'refresh_token',
-                    'refresh_token' => $refres_token,
+                    'refresh_token' => $update_token,
                 ])
                 ->json();
-        } else {
-            $auth = Http::asForm()
-                ->withBasicAuth(config('services.sicredi.client_id'), config('services.sicredi.secret_id'))
-                ->withHeaders([
-                    'x-api-key' => config('services.sicredi.token'),
-                    'context' => 'COBRANCA',
-                ])
-                ->post('https://api-parceiro.sicredi.com.br/auth/openapi/token', [
-                    'username' => $codigoBeneficiario . $codigocooperativa,
-                    'password' => 'DB48A4120D058F35EBD28158500FC9349E51A923C48FF464F3A93FAE1D2E35AF',
-                    'grant_type' => 'password',
-                    'scope' => 'cobranca',
-                ])
-                ->json();
-            // $auth['data_criacao'] = Carbon::now();
+            return $auth;
         }
+
+        $auth = Http::asForm()
+            ->withBasicAuth(config('services.sicredi.client_id'), config('services.sicredi.secret_id'))
+            ->withHeaders([
+                'x-api-key' => config('services.sicredi.token'),
+                'context' => 'COBRANCA',
+            ])
+            ->post('https://api-parceiro.sicredi.com.br/auth/openapi/token', [
+                'username' => $codigoBeneficiario . $codigocooperativa,
+                'password' => 'DB48A4120D058F35EBD28158500FC9349E51A923C48FF464F3A93FAE1D2E35AF',
+                'grant_type' => 'password',
+                'scope' => 'cobranca',
+            ])
+            ->json();
 
         return $auth;
     }
@@ -45,38 +46,24 @@ class SicredApiHelper
     public static function boletoLiquidadoDia($codigoBeneficiario, $codigocooperativa, $posto, $dia)
     {
 
-        dd(session('teste'));
-        $auth = SicredApiHelper::auth($codigoBeneficiario, $codigocooperativa);
-        // dd(session()->all());
-        // $horaAtual = Carbon::now();
-        // $fimToken = session('auth_sicred')['data_criacao']->addSecond(session('auth_sicred')['expires_in']);
-        // $fimTokenRefresh = session('auth_sicred')['data_criacao']->addSecond(session('auth_sicred')['refresh_expires_in']);
-        // $tempoSessaoToken = $horaAtual->diffInSeconds($fimToken);
+        if (Cache::get('access_token')) {
+            $access_token = Cache::get('access_token');
+        } elseif (Cache::get('refresh_token')) {
+            $auth = SicredApiHelper::auth($codigoBeneficiario, $codigocooperativa,Cache::get('refresh_token'));
+            Cache::put('access_token', $auth['access_token'], $seconds = $auth['expires_in']);
+            $access_token = Cache::get('access_token');
+        } else {
+            $auth = SicredApiHelper::auth($codigoBeneficiario, $codigocooperativa);
+            $access_token = Cache::get('access_token');
+            Cache::put('access_token', $auth['access_token'], $seconds = $auth['expires_in']);
+            Cache::put('refresh_token', $auth['refresh_token'], $seconds = $auth['refresh_expires_in']);
+        }
 
-
-        // if (session('auth_sicred')) {
-        //     session('auth_sicred')['data_criacao'] = 1;
-        //     $horaAtual = Carbon::now();
-        //     dd(session('auth_sicred')['data_criacao']->format('d/m/Y H:i:s'), $horaAtual->format('d/m/Y H:i:s'));
-        //
-        //     $tempoSessaoTokenRefresh = $horaAtual->diffInSeconds($fimTokenRefresh);
-        //     if ($tempoSessaoToken < session('auth_sicred')['expires_in']) {
-        //         $auth = session('auth_sicred');
-        //     } elseif ($tempoSessaoTokenRefresh < session('auth_sicred')['refresh_expires_in']) {
-        //         dd($tempoSessaoToken);
-        //         $auth = SicredApiHelper::auth($codigoBeneficiario, $codigocooperativa, session('auth_sicred')['refresh_token']);
-        //         dd($auth, 'Passou no de 1 hora');
-        //     } else {
-        //         $auth = SicredApiHelper::auth($codigoBeneficiario, $codigocooperativa);
-        //     }
-        // } else {
-        //     $auth = SicredApiHelper::auth($codigoBeneficiario, $codigocooperativa);
-        // }
-
+        // dd($access_token);
         $consulta = Http::asForm()
             ->withHeaders([
                 'x-api-key' => config('services.sicredi.token'),
-                'Authorization' => 'bearer ' . $auth['access_token'],
+                'Authorization' => 'bearer ' . $access_token,
                 'cooperativa' => $codigocooperativa,
                 'posto' => $posto,
             ])
@@ -85,7 +72,8 @@ class SicredApiHelper
                 'dia' => $dia,
                 // 'cpfCnpjBeneficiarioFinal' => '36585615000174',
                 // 'pagina' => 1,
-            ])->json();
-        dd($consulta);
+            ])
+            ->json();
+        return $consulta;
     }
 }
--- app/Http/Controllers/SicrediController.php
+++ app/Http/Controllers/SicrediController.php
@@ -15,7 +15,6 @@ class SicrediController extends Controller
      */
     public function index()
     {
-        session(['teste' => date('Y-m-d H:i:s')]);
         $dia = Carbon::now()->subDay(1)->format('d/m/Y');
         $consulta = SicredApiHelper::boletoLiquidadoDia('72334','0703','16',$dia);
         if ($consulta['error']??null) {
@@ -66,76 +65,5 @@ class SicrediController extends Controller
         }
         return "Econtrado ".$nossonumero." Nosso Numero ja cadastrado e não cadastardo no banco de dados";
 
-    }
-
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('Testes.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(TesteCreateRequest $request)
-    {
-
-
-        $dados = $request->all();
-        //dd($dados);
-
-        Teste::create($dados);
-
-        return redirect(route('Testes.index'));
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        $cadastro = Teste::find($id);
-
-        return view('Testes.show', compact('cadastro'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        $cadastro = Teste::find($id);
-        // dd($cadastro);
-
-        return view('Testes.edit', compact('cadastro'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-
-        $cadastro = Teste::find($id);
-
-        $cadastro->fill($request->all());
-        //dd($dados);
-
-        $cadastro->save();
-
-        return redirect(route('Testes.index'));
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        $cadastro = Teste::find($id);
-        $cadastro->delete();
-        return redirect(route('Testes.index'));
     }
 }
