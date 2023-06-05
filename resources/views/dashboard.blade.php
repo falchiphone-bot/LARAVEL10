3c98c338fab804fc5b72b5ef896527502c9d967a
falchiprf <pedroroberto@falchi.com.br>
2023-06-05 07:29:56 -0300

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
                     <div class="badge bg-warning text-wrap"
                         style="width: 100%; height: 50px;
                     ; font-size: 24px;align=˜Center˜ ">
-                        Menu Principal do sistema administrativo e contábil - versão: 31.05.2023 19:38
+                        Menu Principal do sistema administrativo e contábil - versão: 05.06.2023 07:29
                     </div>
                 </div>
                 <div class="card-body">
