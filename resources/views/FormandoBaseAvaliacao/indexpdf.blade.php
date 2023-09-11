3c4d8ebac073163987eb77da08ca1eb89f34bcb0
falchiprf <pedroroberto@falchi.com.br>
2023-09-10 22:13:22 -0300

    Update indexpdf.blade.php

--- resources/views/FormandoBaseAvaliacao/indexpdf.blade.php
+++ resources/views/FormandoBaseAvaliacao/indexpdf.blade.php
@@ -1,28 +1,4 @@
 
-
-        <div class="card">
-            <div class="badge bg-primary text-wrap" style="width: 100%;font-size: 24px;lign=˜Center˜">
-                AVALIAÇÃO DOS FORMANDOS/ATLETAS
-            </div>
-        </div>
-
-
-        <div class="card-body">
-
-
-            <nav class="navbar navbar-red" style="background-color: hsla(234, 92%, 47%, 0.096);">
-
-
-                <div class="card-header">
-                    <div class="badge bg-info text-wrap" style="width: 100%;font-size: 24px">
-                        <p>Total de avaliações cadastrados:
-                            {{ $model->count() ?? 0 }}
-                        </p>
-                    </div>
-                </div>
-
-        </div>
- 
 <head>
     <style>
         /* Estilo para a tabela */
@@ -33,47 +9,68 @@
 
         /* Estilo para as células do cabeçalho */
         th {
-            background-color: #007BFF; /* Cor de fundo do cabeçalho */
-            color: white; /* Cor do texto do cabeçalho */
-            border: 2px solid #333; /* Borda do cabeçalho */
-            padding: 10px; /* Espaçamento interno do cabeçalho */
+            background-color: #007BFF;
+            /* Cor de fundo do cabeçalho */
+            color: white;
+            /* Cor do texto do cabeçalho */
+            border: 2px solid #333;
+            /* Borda do cabeçalho */
+            padding: 10px;
+            /* Espaçamento interno do cabeçalho */
         }
 
         /* Estilo para as células dos dados */
         td {
-            background-color: #F2F2F2; /* Cor de fundo das células de dados */
-            border: 1px solid #333; /* Borda das células de dados */
-            padding: 8px; /* Espaçamento interno das células de dados */
+            background-color: #F2F2F2;
+            /* Cor de fundo das células de dados */
+            border: 1px solid #333;
+            /* Borda das células de dados */
+            padding: 8px;
+            /* Espaçamento interno das células de dados */
         }
     </style>
 </head>
+
 <body>
+Gerado em {{ \Carbon\Carbon::now()->format('d/m/Y H:i:s') }}
+    <table class="table" style="background-color: rgb(247, 247, 213);">
+        <thead>
 
-<table class="table" style="background-color: rgb(247, 247, 213);">
-                <thead>
-                    <tr>
-                        <th scope="col" class="px-6 py-4">DATA</th>
-                        <th scope="col" class="px-6 py-4">NOTA</th>
-                        <th scope="col" class="px-6 py-4">NOME</th>
+            <tr>
+                <th>
+                    AVALIAÇÃO DOS FORMANDOS/ATLETAS
+                </th>
+                <th>
+                    Total de avaliações cadastrados
 
-                    </tr>
-                </thead>
+                </th>
+                <th>
+                    {{ $model->count() ?? 0 }}
+                </th>
+            </tr>
+            <tr>
+                <th scope="col" class="px-6 py-4">DATA</th>
+                <th scope="col" class="px-6 py-4">NOTA</th>
+                <th scope="col" class="px-6 py-4">NOME</th>
 
-                <tbody>
-                    @foreach ($model as $Model)
-                    <tr>
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach ($model as $Model)
+            <tr>
 
 
-                        <td>{{ \Carbon\Carbon::parse($Model->created_at)->format('d/m/Y') }}</td>
+                <td>{{ \Carbon\Carbon::parse($Model->created_at)->format('d/m/Y') }}</td>
 
-                        <td> {{ number_format($Model->avaliacao,2) }} </td>
-                        <td>
-                                      {{ $Model->MostraFormando->nome }}
-                        </td>
+                <td> {{ number_format($Model->avaliacao,2) }} </td>
+                <td>
+                    {{ $Model->MostraFormando->nome }}
+                </td>
 
-                        @endforeach
-                </tbody>
-            </table>
+                @endforeach
+        </tbody>
+    </table>
+    Gerado em {{ \Carbon\Carbon::now()->format('d/m/Y H:i:s') }}
 
 </body>
-
