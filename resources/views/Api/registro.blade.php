9454502b3af46506d99f037dcee87e6bab3a2c79
falchiprf <pedroroberto@falchi.com.br>
2023-11-01 12:54:46 -0300

    Update registro.blade.php

--- resources/views/Api/registro.blade.php
+++ resources/views/Api/registro.blade.php
@@ -96,20 +96,44 @@
 <table>
 
     <tr>
-        @if($model->url_arquivo == null)
+        @if ($model->messagesType == 'image')
 
-                <td>Imagem ID:</td>
-                <td>{{ $model->image_id }}</td>
-                <a href="{{ route('whatsapp.Pegar_URL_Arquivo', $model->image_id) }}"
-                                        class="btn btn-warning" tabindex="-1" role="button" aria-disabled="true">Ver arquivo</a>
+            @if($model->url_arquivo == null)
 
-            @else
+                    <td>Imagem ID:</td>
+                    <td>{{ $model->image_id }}</td>
+                    <a href="{{ route('whatsapp.Pegar_URL_Arquivo', $model->image_id) }}"
+                                            class="btn btn-warning" tabindex="-1" role="button" aria-disabled="true">Ver arquivo</a>
 
-                <td>Imagem</td>
+                @else
 
-                <img src="{{ $model->url_arquivo }}" alt="Imagem JPG">
+                    <td>Imagem</td>
+                    <a href="{{ '../'.$model->url_arquivo }}" target="_blank" style="display: block; text-align: center;">
+                        <img src="{{ '../'.$model->url_arquivo }}" alt="Imagem JPG" width="300" height="300" style="display: block; margin: 0 auto;">
+                    </a>
 
+            @endif
+        @endif
+    </tr>
 
+    <tr>
+        @if ($model->messagesType == 'document')
+
+            @if($model->url_arquivo == null)
+
+                    <td>Imagem ID:</td>
+                    <td>{{ $model->image_id }}</td>
+                    <a href="{{ route('whatsapp.Pegar_URL_Arquivo', $model->image_id) }}"
+                                            class="btn btn-warning" tabindex="-1" role="button" aria-disabled="true">Ver arquivo</a>
+
+                @else
+
+                    <td>Imagem</td>
+                    <a href="{{ '../'.$model->url_arquivo }}" target="_blank" style="display: block; text-align: center;">
+                        <img src="{{ '../'.$model->url_arquivo }}" alt="Imagem JPG" width="300" height="300" style="display: block; margin: 0 auto;">
+                    </a>
+
+            @endif
         @endif
     </tr>
 
@@ -236,23 +260,6 @@
     <td>Tipo da imagem:</td>
     <td>{{ $model->image_mime_type}}</td>
   </tr>
-
-    <tr>
-        @if($model->url_arquivo == null)
-
-                <td>Imagem ID:</td>
-                <td>{{ $model->image_id }}</td>
-                <a href="{{ route('whatsapp.Pegar_URL_Arquivo', $model->image_id) }}"
-                                        class="btn btn-warning" tabindex="-1" role="button" aria-disabled="true">Ver arquivo</a>
-
-            @else
-
-                <td>Imagem</td>
-                <td>{{ $model->url_arquivo }}</td>
-
-        @endif
-    </tr>
-
 
 
   <tr>
