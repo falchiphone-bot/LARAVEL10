629c452ee5359a75a2b027c2db598e7f3edc3ad8
falchiprf <pedroroberto@falchi.com.br>
2023-11-18 11:25:35 -0300

    Update incluirdocumentos.blade.php

--- resources/views/Api/incluirdocumentos.blade.php
+++ resources/views/Api/incluirdocumentos.blade.php
@@ -1,4 +1,4 @@
-@can('LANCAMENTOS DOCUMENTOS - INCLUIR')
+{{-- @can('LANCAMENTOS DOCUMENTOS - INCLUIR') --}}
     @if (session('googleUserDrive'))
         <nav class="navbar navbar-red" style="background-color: hsla(234, 92%, 47%, 0.096);">
             <a class="btn btn-primary" href="/drive/UploadArquivo">Upload de arquivo para
@@ -10,4 +10,4 @@
                 para armazenar no drive</a>
         </nav>
     @endif
-@endcan
+{{-- @endcan --}}
