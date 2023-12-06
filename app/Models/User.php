e569e8eb547914caae7ab9d4a6ac6f40d9bf6387
falchiprf <pedroroberto@falchi.com.br>
2023-12-06 08:37:00 -0300

    AJUSTES CADASTRO USUARIO PARA WHATSAPP

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = ['name', 'email', 'password', 'google_id'];
+    protected $fillable = ['name', 'email', 'password', 'google_id', 'whatsapp'];
 
     /**
      * The attributes that should be hidden for serialization.
--- resources/views/Users/campos.blade.php
+++ resources/views/Users/campos.blade.php
@@ -15,7 +15,7 @@
             <div class="row">
                 <div class="col-6">
                     <label for="email">Email</label>
-                    <input class="form-control @error('Cnpj') is-invalid @else is-valid @enderror" name="email"
+                    <input class="form-control @error('email') is-invalid @else is-valid @enderror" name="email"
                         type="text" id="email" value="{{$cadastro->email??null}}">
                         @error('email')
                         <div class="alert alert-danger">{{ $message }}</div>
@@ -23,11 +23,21 @@
                 </div>
             </div>
 
-            
+            <div class="row">
+                <div class="col-6">
+                    <label for="whatsapp">WhatsApp</label>
+                    <input class="form-control @error('whatsapp') is-invalid @else is-valid @enderror" name="whatsapp"
+                        type="text" id="whatsapp" value="{{$cadastro->whatsapp??null}}">
+                        @error('whatsapp')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+
 
             <div class="row mt-2">
                 <div class="col-6">
-                    <button class="btn btn-primary">Salvar</button>
+                    <button class="btn btn-primary">Salvar edição do usuário</button>
                     <a href="{{route('Usuarios.index')}}" class="btn btn-warning">Retornar para lista</a>
                 </div>
             </div>
