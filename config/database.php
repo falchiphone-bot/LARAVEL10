fcc3a22c7ac095feb913d29eb2108a90770a9ffe
falchiprf <pedroroberto@falchi.com.br>
2024-02-02 11:52:03 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -63,6 +63,29 @@ return [
                 : [],
         ],
 
+        'ixc' => [
+            'driver' => 'mysql',
+            'url' => env('IXC_DATABASE_URL'),
+            'host' => env('IXC_DB_HOST', '127.0.0.1'),
+            'port' => env('IXC_DB_PORT', '3306'),
+            'database' => env('IXC_DB_DATABASE', 'forge'),
+            'username' => env('IXC_DB_USERNAME', 'forge'),
+            'password' => env('IXC_DB_PASSWORD', ''),
+            'unix_socket' => env('IXC_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::ATTR_EMULATE_PREPARES => true
+            ]) : [],
+        ],
+
+
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
