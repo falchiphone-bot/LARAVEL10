1d94b6c23e333d0a7f7c7a9697f476270c660499
falchiprf <pedroroberto@falchi.com.br>
2024-03-18 10:57:14 -0300

    CALCULO DE POR BOLETOS POR CIDADE

--- app/Http/Controllers/ReceberIxcController.php
+++ app/Http/Controllers/ReceberIxcController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Ixc\Cidade;
 use App\Models\Ixc\ReceberIxc;
 use App\Models\Ixc\ClientIxc;
+use App\Models\Ixc\Radpop;
 use DateTime;
 use Exception;
 use Illuminate\Support\Facades\Validator;
@@ -33,28 +34,50 @@ class ReceberIxcController extends Controller
                 $data_vencimento_inicial = now()->format('Y-m-d');
                 $data_vencimento_final = now()->endOfMonth()->format('Y-m-d');
 
+                $Radpop = Radpop::get();
+
+
+            foreach ($Radpop as $pop) {
+
+
+
                 // Obtendo o ID da cidade
-$Cidade = cidade::where('nome', 'like', 'Alvares Florence%')->first()->id;
+                // $Cidade = cidade::where('nome', 'like', 'Valentim Gentil%')->first()->id;
+                $Cidade = $pop->id_cidade;
 
-// Obtendo os clientes da cidade
-$clienteixc = ClientIxc::where('cidade', $Cidade)->limit(1000)->get();
+                // Obtendo os clientes da cidade
+                $clienteixc = ClientIxc::where('cidade', $Cidade)->limit(20000)->get();
 
-// Obtendo os recebimentos dentro do intervalo de datas para clientes da cidade
-$receber = ReceberIxc::whereBetween('data_vencimento', [$data_vencimento_inicial, $data_vencimento_final])
-            ->where('status', 'A')
-            ->whereHas('client', function($query) use ($Cidade) {
-                $query->where('cidade', $Cidade);
-            })
-            ->orderBy('data_vencimento', 'asc')
-            ->get();
+                // Obtendo os recebimentos dentro do intervalo de datas para clientes da cidade
+                $receber = ReceberIxc::whereBetween('data_vencimento', [$data_vencimento_inicial, $data_vencimento_final])
+                            ->where('status', 'A')
+                            ->whereHas('client', function($query) use ($Cidade) {
+                                $query->where('cidade', $Cidade);
+                            })
+                            ->orderBy('data_vencimento', 'asc')
+                            ->get();
 
-
-
-
+                //  dd($Cidade, $clienteixc->Count() ,$clienteixc, $receber->sum('valor'));
 
 
-            dd($Cidade, $clienteixc->Count() ,$clienteixc, $receber->sum('valor'));
+                // Supondo que $Cidade, $clienteixc, e $receber estejam definidos anteriormente
 
+                // Obtenha os valores das variáveis
+                $count = $clienteixc->Count();
+                $sum = $receber->sum('valor');
+
+                // Forme o array com as variáveis
+                $array = array(
+                    "Cidade" => $Cidade,
+                    "Count" => $count,
+                    "Sum" => $sum
+                );
+
+                // Exemplo de uso do array formado
+                echo $array["Cidade"] . ", " . $array["Count"] . ", " . $array["Sum"] . "<br>";
+
+              
+             }
         // return view('Ixc/Clientes.index',compact('receber',));
     }
 
---
+++ app/Models/Ixc/Radpop.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Models\Ixc;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Radpop extends Model
+{
+    protected $table = 'radpop';
+    public $connection = 'ixc';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'pop', 'id_cidade',
+    ];
+
+
+    protected $casts = [
+        'pop' => 'string',
+
+    ];
+
+}
