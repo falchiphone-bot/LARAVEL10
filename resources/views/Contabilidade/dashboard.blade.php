cda91949d43f8c855db7f8f3b5b078aa5a4f4610
falchiprf <pedroroberto@falchi.com.br>
2024-04-30 08:13:10 -0300

    Update dashboard.blade.php

--- resources/views/Contabilidade/dashboard.blade.php
+++ resources/views/Contabilidade/dashboard.blade.php
@@ -93,6 +93,13 @@
                                     <a class="btn btn-success" href="/PlanoContas/pesquisaavancada">Lançamentos contábeis</a>
                                 </nav>
 
+
+                                    <nav class="navbar navbar-red" style="background-color: hsla(234, 92%, 47%, 0.096);">
+                                        <a class="btn btn-primary" href="/Empresas">Empresas</a>
+                                    </nav>
+
+                         
+
                                 <nav class="navbar navbar-red" style="background-color: hsla(234, 92%, 47%, 0.096);">
                                     <a class="btn btn-success" href="/LeituraArquivo">Abrir arquivo planilha</a>
                                 </nav>
