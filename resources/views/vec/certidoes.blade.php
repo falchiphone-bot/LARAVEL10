d2cb7dc0562d24e01e3a4e00f5d4eda339ec9f7f
falchiprf <pedroroberto@falchi.com.br>
2025-04-08 12:17:00 -0300

    certidoes atualizadas

--- resources/views/vec/certidoes.blade.php
+++ resources/views/vec/certidoes.blade.php
@@ -29,53 +29,18 @@
 
     <section class="bg-gray-800 py-12 px-6">
         <div class="max-w-4xl mx-auto">
-            <h3 class="text-3xl font-bold text-center text-red-500">CERTIDÃO NEGATIVA DE DÉBITOS RELATIVOS AOS TRIBUTOS FEDERAIS E À DÍVIDA ATIVA DA UNIÃO DO VOTUPORANGA ESPORTE CLUBE</h3>
+            <h3 class="text-3xl font-bold text-center text-red-500">CERTIDÃO NEGATIVA DE DÉBITOS RELATIVOS AOS TRIBUTOS FEDERAIS E À DÍVIDA ATIVA DA UNIÃO DO VOTUPORANGA ESPORTE CLUBE - VALIDADE 29.09.2025</h3>
             <p class="text-green-300 text-lg text-center mt-4">
-                <a href="{{ route('download', ['id_arquivo' => 40680]) }}">Baixar Arquivo PDF</a>
+                <a href="{{ route('download', ['id_arquivo' => 40680]) }}" target="_blank" rel="noopener noreferrer">Baixar Arquivo PDF</a>
             </p>
         </div>
     </section>
 
     <section class="bg-gray-800 py-12 px-6">
         <div class="max-w-4xl mx-auto">
-            <h3 class="text-3xl font-bold text-center text-red-500">CERTIDÃO NEGATIVA DE DÉBITOS - Nº 7894/2025 DO VOTUPORANGA ESPORTE CLUBE</h3>
+            <h3 class="text-3xl font-bold text-center text-red-500">CERTIDÃO NEGATIVA DE DÉBITOS - Nº 7894/2025 DO VOTUPORANGA ESPORTE CLUBE - VALIDADE 02.10.2025</h3>
             <p class="text-green-300 text-lg text-center mt-4">
-                <a href="{{ route('download', ['id_arquivo' => 40681]) }}">Baixar Arquivo PDF</a>
-            </p>
-        </div>
-    </section>
-
-
-
-
-    <section class="bg-gray-800 py-12 px-6">
-        <div class="max-w-4xl mx-auto">
-            <h3 class="text-3xl font-bold text-center text-red-500">CERTIDÃO NEGATIVA DE DÉBITOS TRABALHISTAS DO VOTUPORANGA ESPORTE CLUBE</h3>
-            <p class="text-green-300 text-lg text-center mt-4">
-                <a href="{{ route('download', ['id_arquivo' => 40682]) }}">Baixar Arquivo PDF</a>
-            </p>
-        </div>
-    </section>
-
-    <section class="bg-gray-800 py-12 px-6">
-        <div class="max-w-4xl mx-auto">
-            <h3 class="text-3xl font-bold text-center text-red-500">Certidão Negativa de Débitos Inscritos da Dívida Ativa do Estado de São Paulo do VOTUPORANGA ESPORTE CLUBE</h3>
-            <p class="text-green-300 text-lg text-center mt-4">
-                <a href="{{ route('download', ['id_arquivo' => 40683]) }}">Baixar Arquivo PDF</a>
-            </p>
-        </div>
-    </section>
-
-
-
-
-
-
-    <section class="bg-gray-800 py-12 px-6">
-        <div class="max-w-4xl mx-auto">
-            <h3 class="text-3xl font-bold text-center text-red-500">Certificado de Regularidade do FGTS - CRF DO VOTUPORANGA ESPORTE CLUBE</h3>
-            <p class="text-green-300 text-lg text-center mt-4">
-                <a href="{{ route('download', ['id_arquivo' => 40684]) }}">Baixar Arquivo PDF</a>
+                <a href="{{ route('download', ['id_arquivo' => 40681]) }}" target="_blank" rel="noopener noreferrer">Baixar Arquivo PDF</a>
             </p>
         </div>
     </section>
@@ -85,9 +50,18 @@
 
     <section class="bg-gray-800 py-12 px-6">
         <div class="max-w-4xl mx-auto">
-            <h3 class="text-3xl font-bold text-center text-red-500">PODER JUDICIÁRIO JUSTIÇA FEDERAL TRIBUNAL REGIONAL FEDERAL DA 3a REGIÃO CERTIDÃO JUDICIAL CÍVEL DO VOTUPORANGA ESPORTE CLUBE</h3>
+            <h3 class="text-3xl font-bold text-center text-red-500">CERTIDÃO NEGATIVA DE DÉBITOS TRABALHISTAS DO VOTUPORANGA ESPORTE CLUBE - VALIDADE 29.09.2025</h3>
             <p class="text-green-300 text-lg text-center mt-4">
-                <a href="{{ route('download', ['id_arquivo' => 40685]) }}">Baixar Arquivo PDF</a>
+                <a href="{{ route('download', ['id_arquivo' => 40682]) }}" target="_blank" rel="noopener noreferrer">Baixar Arquivo PDF</a>
+            </p>
+        </div>
+    </section>
+
+    <section class="bg-gray-800 py-12 px-6">
+        <div class="max-w-4xl mx-auto">
+            <h3 class="text-3xl font-bold text-center text-red-500">Certidão Negativa de Débitos Inscritos da Dívida Ativa do Estado de São Paulo do VOTUPORANGA ESPORTE CLUBE - VALIDADE 02.05.2025</h3>
+            <p class="text-green-300 text-lg text-center mt-4">
+                <a href="{{ route('download', ['id_arquivo' => 40683]) }}" target="_blank" rel="noopener noreferrer">Baixar Arquivo PDF</a>
             </p>
         </div>
     </section>
@@ -99,9 +73,35 @@
 
     <section class="bg-gray-800 py-12 px-6">
         <div class="max-w-4xl mx-auto">
-            <h3 class="text-3xl font-bold text-center text-red-500"> Débitos Tributários Não Inscritos na Dívida Ativa do Estado de São Paulo</h3>
+            <h3 class="text-3xl font-bold text-center text-red-500">Certificado de Regularidade do FGTS - CRF DO VOTUPORANGA ESPORTE CLUBE - VALIDADE 14.04.2025</h3>
             <p class="text-green-300 text-lg text-center mt-4">
-                <a href="{{ route('download', ['id_arquivo' => 40686]) }}">Baixar Arquivo PDF</a>
+                <a href="{{ route('download', ['id_arquivo' => 40684]) }}" target="_blank" rel="noopener noreferrer">Baixar Arquivo PDF</a>
+            </p>
+        </div>
+    </section>
+
+
+
+
+    <section class="bg-gray-800 py-12 px-6">
+        <div class="max-w-4xl mx-auto">
+            <h3 class="text-3xl font-bold text-center text-red-500">PODER JUDICIÁRIO JUSTIÇA FEDERAL TRIBUNAL REGIONAL FEDERAL DA 3a REGIÃO CERTIDÃO JUDICIAL CÍVEL DO VOTUPORANGA ESPORTE CLUBE - VALIDADE 02.07.2025</h3>
+            <p class="text-green-300 text-lg text-center mt-4">
+                <a href="{{ route('download', ['id_arquivo' => 40685]) }}" target="_blank" rel="noopener noreferrer">Baixar Arquivo PDF</a>
+            </p>
+        </div>
+    </section>
+
+
+
+
+
+
+    <section class="bg-gray-800 py-12 px-6">
+        <div class="max-w-4xl mx-auto">
+            <h3 class="text-3xl font-bold text-center text-red-500"> Débitos Tributários Não Inscritos na Dívida Ativa do Estado de São Paulo - VALIDADE 02.10.2025</h3>
+            <p class="text-green-300 text-lg text-center mt-4">
+                <a href="{{ route('download', ['id_arquivo' => 40686]) }}" target="_blank" rel="noopener noreferrer">Baixar Arquivo PDF</a>
             </p>
         </div>
     </section>
@@ -109,15 +109,11 @@
 
     <section class="bg-gray-800 py-12 px-6">
         <div class="max-w-4xl mx-auto">
-            <h3 class="text-3xl font-bold text-center text-red-500">PODER JUDICIÁRIO JUSTIÇA FEDERAL TRIBUNAL REGIONAL FEDERAL DA 3a REGIÃO CERTIDÃO JUDICIAL CRIMINAL NEGATIVA DO VOTUPORANGA ESPORTE CLUBE</h3>
+            <h3 class="text-3xl font-bold text-center text-red-500">PODER JUDICIÁRIO JUSTIÇA FEDERAL TRIBUNAL REGIONAL FEDERAL DA 3a REGIÃO CERTIDÃO JUDICIAL CRIMINAL NEGATIVA DO VOTUPORANGA ESPORTE CLUBE - VALIDADE 02.07.2025</h3>
             <p class="text-green-300 text-lg text-center mt-4">
-                <a href="{{ route('download', ['id_arquivo' => 40687]) }}">Baixar Arquivo PDF</a>
+                <a href="{{ route('download', ['id_arquivo' => 40687]) }}" target="_blank" rel="noopener noreferrer">Baixar Arquivo PDF</a>
             </p>
         </div>
     </section>
-
-
-
-
 </body>
 </html>
