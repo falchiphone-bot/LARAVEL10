2ec86e8fd40c6d0a2a66c36f30b9f47655789902
falchiprf <pedroroberto@falchi.com.br>
2025-04-15 18:35:10 -0300

    SUB 15 E SUB 17

--- resources/views/tanabisaf/categoriasub17.blade.php
+++ resources/views/tanabisaf/categoriasub17.blade.php
@@ -11,12 +11,21 @@
 <body class="bg-green-900  text-white min-h-screen flex flex-col items-center pt-32 px-4">
 
   <table class="w-full max-w-md bg-green shadow-lg rounded-xl overflow-hidden">
+
+
+    <caption class="text-xl font-bold text-blue-900 bg-blue-100 py-4">
+        <a href="https://www.futebolpaulista.com.br/Competicoes/Tabela.aspx" class="text-blue-900 hover:underline" target="_blank" rel="noopener noreferrer">
+          Para ver a tabela do Campeonato Paulista selecione a competição
+        </a>
+      </caption>
+
+
     <caption class="text-xl font-bold text-blue-900 bg-blue-100 py-4">
       Paulista SUB17 - 2025 | Rodada 01
     </caption>
     <tr class="border-b">
       <th class="bg-gray-500 text-left px-6 py-4 w-1/3">Jogo</th>
-      <td class="px-6 py-4">Santa Fé FC vs Tanabi SAF</td>
+      <td class="px-6 py-4">Santa Fé FC 2 X 0 Tanabi SAF</td>
     </tr>
     <tr class="border-b">
       <th class="bg-gray-500 text-left px-6 py-4">Data</th>
@@ -26,7 +35,17 @@
       <th class="bg-gray-500 text-left px-6 py-4">Horário</th>
       <td class="px-6 py-4">11:00</td>
     </tr>
+
     <tr class="border-b">
       <th class="bg-gray-500 text-left px-6 py-4">Estádio</th>
       <td class="px-6 py-4">Estadio Municipal Evandro de Paula / Santa Fé do Sul</td>
-      <td class="px-6 py-
+    </tr>
+
+       <tr class="border-b">
+        <th class="bg-gray-500 text-left px-6 py-4">Súmula</th>
+
+        <td class="px-6 py-4">
+        <p class="text-white-900 text-lg text-center mt-4">
+            <a href="{{ route('download', ['id_arquivo' => 40734]) }}"target="_blank" rel="noopener noreferrer" >Baixar Arquivo PDF do jogo</a>
+        </p>
+
