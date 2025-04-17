3227cd60b8f91c0e65b4b6f5b96f6bad1eba2d67
falchiprf <pedroroberto@falchi.com.br>
2025-04-17 11:19:03 -0300

    RODADA 12042025 -SUB 15 E 17

--- resources/views/tanabisaf/categoriasub17.blade.php
+++ resources/views/tanabisaf/categoriasub17.blade.php
@@ -20,32 +20,5 @@
       </caption>
 
 
-    <caption class="text-xl font-bold text-blue-900 bg-blue-100 py-4">
-      Paulista SUB17 - 2025 | Rodada 01
-    </caption>
-    <tr class="border-b">
-      <th class="bg-gray-500 text-left px-6 py-4 w-1/3">Jogo</th>
-      <td class="px-6 py-4">Santa Fé FC 2 X 0 Tanabi SAF</td>
-    </tr>
-    <tr class="border-b">
-      <th class="bg-gray-500 text-left px-6 py-4">Data</th>
-      <td class="px-6 py-4">12/04/2025</td>
-    </tr>
-    <tr class="border-b">
-      <th class="bg-gray-500 text-left px-6 py-4">Horário</th>
-      <td class="px-6 py-4">11:00</td>
-    </tr>
-
-    <tr class="border-b">
-      <th class="bg-gray-500 text-left px-6 py-4">Estádio</th>
-      <td class="px-6 py-4">Estadio Municipal Evandro de Paula / Santa Fé do Sul</td>
-    </tr>
-
-       <tr class="border-b">
-        <th class="bg-gray-500 text-left px-6 py-4">Súmula</th>
-
-        <td class="px-6 py-4">
-        <p class="text-white-900 text-lg text-center mt-4">
-            <a href="{{ route('download', ['id_arquivo' => 40734]) }}"target="_blank" rel="noopener noreferrer" >Baixar Arquivo PDF do jogo</a>
-        </p>
+      @include('tanabisaf.jogos.2025.sub17.rodada01-12042025')
 
