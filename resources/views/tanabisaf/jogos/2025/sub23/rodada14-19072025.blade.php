b07e6407d4bbd72842fac33e55735af177f9c924
falchiprf <falchiphone@gmail.com>
2025-07-25 09:43:38 -0300

    SUB 23

--- resources/views/tanabisaf/jogos/2025/sub23/rodada14-19072025.blade.php
+++ resources/views/tanabisaf/jogos/2025/sub23/rodada14-19072025.blade.php
@@ -29,7 +29,7 @@
       <tbody>
         <tr class="border-b border-gray-800">
           <th class="bg-gray-600 text-left px-6 py-4 w-1/3">Jogo</th>
-          <td class="px-6 py-4">Tanabi SAF  x  Mauaense</td>
+          <td class="px-6 py-4">Tanabi SAF 0 x 0 Mauaense</td>
         </tr>
         <tr class="border-b border-gray-700">
           <th class="bg-gray-600 text-left px-6 py-4">Data</th>
@@ -46,16 +46,16 @@
 
 
 
-        {{-- <tr>
+        <tr>
           <th class="bg-gray-600 text-left px-6 py-4">Súmula</th>
           <td class="px-6 py-4 text-center">
-            <a href="{{ route('download', ['id_arquivo' => 40901]) }}"
+            <a href="{{ route('download', ['id_arquivo' => 40947]) }}"
                class="text-blue-300 font-semibold hover:text-blue-400 hover:underline transition duration-200"
                target="_blank" rel="noopener noreferrer">
               Baixar Arquivo PDF do jogo
             </a>
           </td>
-        </tr> --}}
+        </tr>
 
         {{-- <tr>
             <th class="bg-gray-600 text-left px-6 py-4">Boletim financeiro</th>
