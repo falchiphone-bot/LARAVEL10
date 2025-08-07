57cf891e4fcceb607004a5a3abcdfc704d672b97
falchiprf <falchiphone@gmail.com>
2025-08-07 10:20:05 -0300

    Update categoriasub23.blade.php

--- resources/views/tanabisaf/jogos/2025/sub23/categoriasub23.blade.php
+++ resources/views/tanabisaf/jogos/2025/sub23/categoriasub23.blade.php
@@ -156,6 +156,15 @@
     </li>
 </ul>
 
+<ul class="text-gray-300 text-lg text-center mt-4">
+    <li>
+        <a href="{{ url('tanabisaf.jogos.2025.sub23.rodada16') }}"
+           class="block p-2 text-blue-400 underline hover:text-red-500">
+            Rodada 16
+        </a>
+    </li>
+</ul>
+
   <footer class="mt-8 text-gray-400 text-sm">
     <p>&copy; 2025 TANABI SAF. Todos os direitos reservados.</p>
     <p>Desenvolvido por <a href="
